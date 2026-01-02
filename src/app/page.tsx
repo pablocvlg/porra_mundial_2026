dba234c5204dc18073356bf928731bad4c7c7e0e
Pablo Casado Vela <pablo.casado@inycom.es>
2026-01-02 12:51:23 +0100

    Initial Next.js + Tailwind setup

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="p-10">
+      <h1 className="text-3xl font-bold text-red-600">
+        Tailwind funciona
+      </h1>
+    </main>
+  );
+}
