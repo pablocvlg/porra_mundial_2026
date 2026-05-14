3370816dd779e2643138e98c3752eb8a3110cdd9
Pablo <pablocvlg@gmail.com>
2026-05-14 14:24:29 +0200

    Minor changes to enviar porra

--- src/app/(public)/porra/page.tsx
+++ src/app/(public)/porra/page.tsx
@@ -608,10 +608,12 @@ export default function PorraPage() {
         <div className="max-w-8xl mx-auto p-4">
           <h1 className="text-xl font-bold mb-4">PORRA MUNDIAL 2026</h1>
           <div className="max-w-6xl mx-auto">
-            <div className="bg-gray-900/50 backdrop-blur-md border border-gray-800/50 rounded-lg p-4 mb-4">
+            <div className="bg-gray-900/70 backdrop-blur-md border border-gray-800/50 rounded-lg p-4 mb-4">
               <div className="mt-2 mb-3 ml-9 flex justify-left gap-4">
-                <p className="font-bold text-s text-left text-red-400">
-                  Solo dejará enviar la porra si tu nombre está en la lista. Asegúrate de poner bien tu nombre (incluyendo tildes).
+                <p className="font-bold text-sm text-left text-red-400">
+                  Cualquiera puede probar a rellenar la porra, pero solo dejará enviarla si tu nombre está en la lista de participantes.<br />
+                  Asegúrate de poner tu nombre exáctamente (incluyendo primer apellido y tildes).<br />
+                  Puedes rellenar esta parte antes o después de los resultados.
                 </p>
               </div>
               <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
